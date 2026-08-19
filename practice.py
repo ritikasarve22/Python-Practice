6d01829ab8675ab07ff3a11ff99d1c84485954c7
ritikasarve22 <sarve.ritika1@gmail.com>
2026-08-19 23:25:34 +0530

    🎲 Dice Game

--- practice.py
+++ practice.py
@@ -1,4 +1,4 @@
-import random
+'''import random
 
 choices = ["stone", "paper", "scissors"]
 
@@ -20,4 +20,38 @@ elif user == "scissors" and computer == "paper":
     print("You Win! 🎉")
 
 else:
-    print("Computer Wins! 🤖")
+    print("Computer Wins! 🤖")'''
+
+# Stone Paper Scissor Game 
+import random
+
+choices = ["stone" , "paper" , "scissor"]
+
+computer = random.choice(choices)
+
+user = input("Enter stone , paper or scissor :  ")
+
+print("Computer choice : ",computer)
+
+if user == computer :
+    print("draw🫱🏻‍🫲🏻")
+
+elif user == "stone" and computer == "paper":
+    print("Computer Wins !")
+
+elif user == "paper" and computer == "stone":
+    print("You Win !")
+
+elif user == "paper" and computer == "scissor":
+    print("Computer Wins !")
+
+elif user == "scissor" and computer == "paper":
+    print("You Wins !")
+
+elif user == "stone" and computer == "scissor":
+    print("You Wins !")
+
+else:
+    print("Computer Wins ! ")
+
+
