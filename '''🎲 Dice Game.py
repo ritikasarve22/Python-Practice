72bd374f2842f82357dd6841c8b09b9345108432
ritikasarve22 <sarve.ritika1@gmail.com>
2026-08-19 23:38:34 +0530

    Dice Game

---
+++ '''🎲 Dice Game.py
@@ -0,0 +1,19 @@
+'''🎲 Dice Game 
+The computer rolls a dice with a number between 1 and 6. The user also enters a number.
+Same number → You Win 🎉
+Different number → Computer Wins 🤖'''
+
+import random
+
+choices = [1,2,3,4,5,6]
+
+computer = random.choice(choices)
+
+user = input("Enter your choice = 1,2,3,4,5 or 6 : ")
+
+print("Computer choice :  ",computer)
+
+if user == computer :
+    print("You Win!")
+else:
+    print("Computer Wins!")
