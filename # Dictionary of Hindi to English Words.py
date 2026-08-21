94a2614a78b3155c162acc220d53e78d5631a574
ritikasarve22 <sarve.ritika1@gmail.com>
2026-08-21 21:44:46 +0530

    📘Dictionary of Hindi to English

---
+++ # Dictionary of Hindi to English Words.py
@@ -0,0 +1,14 @@
+# Dictionary of Hindi to English Words
+
+words = {
+    "Paani" : "Water",
+    "Aam" : "Mango",
+    "Ladka" : "Boy",
+    "Ladki" : "Girl",
+    "Pankha" : "Fan",
+    "Khidki" : "Window"
+        }
+
+word = input("Enter a word which you want to transclate : ")
+
+print(words.get(word))
